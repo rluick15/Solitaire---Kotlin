1a67b477a8b0c697993e93a7edfef6411efff488
rluick15 <rluick15@gmail.com>
2016-10-12 19:42:22 -0400

    create deck methods

--- src/Deck.kt
+++ src/Deck.kt
@@ -1,9 +1,19 @@
+import java.util.*
+
 /**
  * Created by rluic on 10/12/2016.
  */
 class Deck {
 
     val cards = Array(52, { Card(it % 13, getSuit(it)) })
+    var cardsInDeck: MutableList<Card> = cards.toArrayList()
+
+    fun drawCard(): Card = cardsInDeck.removeAt(0)
+
+    fun reset() {
+        cardsInDeck = cards.toArrayList()
+        Collections.shuffle(cardsInDeck)
+    }
 
     private fun getSuit(i: Int) = when(i / 13) {
         0 -> "Clubs"
